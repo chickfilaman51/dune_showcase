19e4974102179c517452bb33ec8906288abaf43c
Krish Behera <krishbehera10@gmail.com>
2024-06-01 13:03:09 -0400

    added about

--- sections/About.jsx
+++ sections/About.jsx
@@ -16,25 +16,19 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="| About Metaversus" textStyles="text-center" />
+      <TypingText title="| About Dune" textStyles="text-center" />
 
       <motion.p
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-white">Metaverse</span> is a new
-        thing in the future, where you can enjoy the virtual world by feeling
-        like it's really real, you can feel what you feel in this metaverse
-        world, because this is really the{' '}
+        <span className="font-extrabold text-white">Dune</span> is a novel by
+        Frank Herbert focused on the complex nature of{' '}
         <span className="font-extrabold text-white">
-          madness of the metaverse
+          empires and religion
         </span>{' '}
-        of today, using only{' '}
-        <span className="font-extrabold text-white">VR</span> devices you can
-        easily explore the metaverse world you want, turn your dreams into
-        reality. Let's{' '}
-        <span className="font-extrabold text-white">explore</span> the madness
-        of the metaverse by scrolling down
+        of the planet Dune. It has multiple facets with so called natives on the planet and spice, a material that sustains life and transportation throughout the galaxy.{' '}
+       
       </motion.p>
 
       <motion.img
