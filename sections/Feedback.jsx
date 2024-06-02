614942f4a4cef8adb69719eec22005ffe9647775
Krish Behera <krishbehera10@gmail.com>
2024-06-02 11:04:00 -0400

    added sentiment

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -9,9 +9,15 @@ import { TitleText } from '../components';
 const Feedback = () => (
   <section className={`${styles.paddings}`}>
     <TitleText
-      title={<>Sentiment Analysis of  <br className="md:block hidden" />Dune</>}
-      textStyles="text-center"    
+      title={
+        <>
+          Sentiment Analysis of <br className="md:block hidden" />
+          Dune
+        </>
+      }
+      textStyles="text-center"
     />
+
     <motion.div
       variants={staggerContainer}
       initial="hidden"
@@ -19,33 +25,73 @@ const Feedback = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6 mt-10`}
     >
-      
       <motion.div
-        variants={fadeIn('right', 'tween', 0.2, 1)}
+        variants={fadeIn("right", "tween", 0.2, 1)}
         className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
       >
-        
         <div className="feedback-gradient" />
         <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
             Original Data
           </h4>
-          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
-             
-          </p>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white"></p>
         </div>
 
         <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
-          The following graph shows the original data of the sentiment analysis of the book Dune. The higher the line, the more positive it is. The lower, the more negative.
+          The following graph shows the original data of the sentiment analysis
+          of the book Dune. The higher the line, the more positive it is. The
+          lower, the more negative.
         </p>
       </motion.div>
 
       <motion.div
-        variants={fadeIn('left', 'tween', 0.2, 1)}
+        variants={fadeIn("left", "tween", 0.2, 1)}
         className="relative flex-1 flex justify-center items-center"
       >
         <img
-          src="/planet-09.png"
+          src="/sentimentFit.png"
+          alt="planet-09"
+          className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+        />
+
+        <motion.div
+          variants={zoomIn(0.4, 1)}
+          className="lg:block hidden absolute -left-[10%] top-[3%]"
+        ></motion.div>
+      </motion.div>
+    </motion.div>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row-reverse flex-col gap-6 mt-10`}
+    >
+      <motion.div
+        variants={fadeIn("left", "tween", 0.2, 1)}
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
+      >
+        <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
+            Original Data
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white"></p>
+        </div>
+
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
+          The following graph shows the original data of the sentiment analysis
+          of the book Dune. The higher the line, the more positive it is. The
+          lower, the more negative.
+        </p>
+      </motion.div>
+
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="relative flex-1 flex justify-center items-center"
+      >
+        <img
+          src="/sentiment.png"
           alt="planet-09"
           className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
         />
@@ -54,7 +100,7 @@ const Feedback = () => (
           variants={zoomIn(0.4, 1)}
           className="lg:block hidden absolute -left-[10%] top-[3%]"
         >
-     
+          {/* ... */}
         </motion.div>
       </motion.div>
     </motion.div>
