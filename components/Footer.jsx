81b0947a14db3711488459dd1c5e075cba29df8b
Krish Behera <krishbehera10@gmail.com>
2024-06-02 11:32:22 -0400

    fixed foot final comit link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,15 +19,20 @@ const Footer = () => (
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
           Enter the Dune Universe
         </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+        <button
+          type="button"
+          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
+        >
           <img
             src="/headset.svg"
             alt="headset"
             className="w-[24px] h-[24px] object-contain"
           />
-          <span className="font-normal text-[16px] text-white">
-          Enter the Dune Universe
-          </span>
+          <a href="https://dune.fandom.com/wiki/Dune_Wiki">
+            <span className="font-normal text-[16px] text-white">
+              Enter the Dune Universe
+            </span>
+          </a>
         </button>
       </div>
 
@@ -41,9 +46,6 @@ const Footer = () => (
           <p className="font-normal text-[14px] text-white opacity-50">
             Made By Krish Behera
           </p>
-
-          
-          
         </div>
       </div>
     </div>
